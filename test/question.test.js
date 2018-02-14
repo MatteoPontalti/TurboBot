bff243dd47498854bd637eff4e77702f386b8ac8
Nicholas <nicholas.grigolato@studenti.unitn.it>
2018-02-14 17:52:30 +0100

    Update test.js

--- test/question.test.js
+++ test/question.test.js
@@ -17,7 +17,7 @@ test('Domanda: Quando sono da pagare le tasse?', function(done) {
             expect(response.status).toBe(200);
             return response.json();
         }).then(function(data) {
-        expect(data.Risposta).toBe('Rate e scadenze\n\nLe tasse saranno pagate in tre rate:\n\n- 1 rata: importo fisso per tutti gli studenti, scadenza 30/09/2017 (per gli studenti che si immatricolano ad una Laurea Magistrale, la scadenza della prima rata è entro 7 giorni dalla data di immatricolazione)\n- 2 rata: il 70% dei contributi quota variabile dovuti in base all’ISEE università, scadenza 30/11/2017\n- 3 rata: il 30% dei contributi quota variabile dovuti in base all’ISEE università, scadenza 31/3/2018.\nNei casi in cui la quota variabile dei contributi sia uguale o inferiore a € 500,00, le tasse saranno pagate per intero nella prima e seconda rata: non ci sarà la terza rata tasse.');
+        expect(data.Risposta).toBe('Rate e scadenze\n\nLe tasse saranno pagate in tre rate:\n\n- 1 rata: importo fisso per tutti gli studenti, scadenza 30/09/2017 (per gli studenti che si immatricolano ad una Laurea Magistrale, la scadenza della prima rata è entro 7 giorni dalla data di immatricolazione)\n- 2 rata: il 70% dei contributi quota variabile dovuti in base all’ISEE università, scadenza 30/11/2017\n- 3 rata: il 30% dei contributi quota variabile dovuti in base all’ISEE università, scadenza 31/3/2018.\nNei casi in cui la quota variabile dei contributi sia uguale o inferiore a € 500,00, le tasse saranno pagate per intero nella prima e seconda rata: non ci sarà la terza rata tasse.\nPer maggiori informazioni: https://infostudenti.unitn.it/it/tasse-universitarie');
         done();
     });
 });
@@ -29,7 +29,7 @@ test('Domanda: Come posso iscrivermi all\'università?', function(done) {
             expect(response.status).toBe(200);
             return response.json();
         }).then(function(data) {
-        expect(data.Risposta).toBe('Per iscriversi ai corsi di Laurea in Fisica, Matematica, Informatica, Ingegneria dell’Informazione e delle Comunicazioni, Ingegneria dell’Informazione e Organizzazione d’impresa è necessario sostenere una prova selettiva di ammissione. Sono disponibili una sessione primaverile  ed estiva solitamente ad aprile ed agosto.');
+        expect(data.Risposta).toBe('Per iscriversi ai corsi di Laurea in Fisica, Matematica, Informatica, Ingegneria dell’Informazione e delle Comunicazioni, Ingegneria dell’Informazione e Organizzazione d’impresa è necessario sostenere una prova selettiva di ammissione. Sono disponibili una sessione primaverile  ed estiva solitamente ad aprile ed agosto.\n Per maggiori informazioni:\nAmmissioni:https://infostudenti.unitn.it/it/ammissioni\nImmatricolazioni:https://infostudenti.unitn.it/it/immatricolazioni\nRinnovo iscrizioni:https://infostudenti.unitn.it/it/rinnovo-iscrizioni');
         done();
     });
 });
